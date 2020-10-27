9f3242ed539eec5361ab21dca6a04817af19e6ca
Áslaug Högna <ash10@hi.is>
2020-10-27 21:44:26 +0000

    scripts útfært

--- scripts.js
+++ scripts.js
@@ -11,8 +11,79 @@ function start() {
   alert('Halló!')
 }
 
-// Hér er gott að commenta út til að vinna í encode/decode föllum fyrst og síðan „viðmóti“ forrits
-start();
+do {
+  play();
+} while (confirm('Reyna aftur?'));
+
+function play() {
+  let inp = prompt('Hvort viltu kóða eða afkóða streng? Skrifaðu "kóða" eða "afkóða"');
+
+  if (inp == "kóða") {
+    let input = prompt('Hversu mikið á að hliðra streng? Gefðu upp heiltölu á bilinu [1, 31]');
+
+    let n = Number.parseInt(input, 10);
+    if (n < 1 || n > 31 || isNaN(n) ) {
+      alert(`${input} er ekki heiltala á bilinu [1,31]. Reyndu aftur.`);
+      return;
+    }
+    let str = prompt(`Gefðu upp strenginn sem á að afkóða með hliðrun ${n}:`);
+    if (typeof str === 'string'){
+    let STR = str.toLocaleUpperCase();
+    if (invalid(STR)) {
+      alert(`Þú gafst upp stafi sem ekki er hægt að afkóða: Reyndu aftur.`);
+      return;
+    }
+    encode(STR, n);
+    } else if (str == "") {
+      alert(`Þú gafst ekki upp streng. Reyndu aftur.`);
+      return;
+    }
+  }
+
+  else if (inp == "afkóða") {
+    let input = prompt('Hversu mikið á að hliðra streng? Gefðu upp heiltölu á bilinu [1, 31]');
+
+    let n = Number.parseInt(input, 10);
+    if (n < 1 || n > 31 || isNaN(n) ) {
+      alert(`${input} er ekki heiltala á bilinu [1,31]. Reyndu aftur.`);
+      return;
+    }
+    let str = prompt(`Gefðu upp strenginn sem á að afkóða með hliðrun ${n}:`);
+    let STR = str.toLocaleUpperCase();
+    if (invalid(STR)) {
+      alert(`Þú gafst upp stafi sem ekki er hægt að afkóða: Reyndu aftur.`);
+      return;
+    }
+    decode(STR, n);
+  }
+  
+  else {
+    alert(`Veit ekki hvaða aðgerð "${inp}" er. Reyndu aftur.`);
+    return;
+  }
+}
+
+/**
+ * Athugar hvort einhverjir stafir séu ekki hluti af LETTERS
+ * 
+ * @param {string} STR Strengur sem á að bera saman við LETTERS
+ * @returns {boolean} skilar false ef allir stafir finnast í LETTERS, annars true.
+ */
+function invalid(STR) {
+  let count = 0;
+  for (let i = 0; i < STR.length; i++) {
+    for (let j = 0; j < LETTERS.length; j++) {
+      if (STR[i] == LETTERS[j]) {
+        count++;
+        break;
+      } 
+    }     
+  } 
+  if (count == STR.length) {
+    return false;
+  } else return true;
+}
+
 
 /**
  * Kóðar streng með því að hliðra honum um n stök.
@@ -22,7 +93,22 @@ start();
  * @returns {string} Upprunalegi strengurinn hliðraður um n til hægri
  */
 function encode(str, n) {
-  return str;
+  
+  let str_coded ="";
+
+  for (let i = 0; i < str.length; i++) {
+    let ind = LETTERS.indexOf(str[i]);
+    let newind;
+    
+    if (ind + n < LETTERS.length) {
+    newind = ind + n; 
+    } else {
+    newind = ind + n - LETTERS.length; 
+    }
+    
+    str_coded += (LETTERS[newind]);
+  }
+  alert(str_coded);
 }
 
 /**
@@ -33,7 +119,21 @@ function encode(str, n) {
  * @returns {string} Upprunalegi strengurinn hliðraður um n til vinstri
  */
 function decode(str, n) {
-  return str;
+  let str_decoded ="";
+
+for (let i = 0; i < str.length; i++) {
+  let ind = LETTERS.indexOf(str[i]);
+  let newind;
+  
+  if (ind - n < 0) {
+   newind = ind - n + LETTERS.length; 
+  } else {
+   newind = ind - n; 
+  }
+  
+  str_decoded += (LETTERS[newind]);  
+} 
+  alert(str_decoded);
 }
 
 console.assert(encode('A', 3) === 'D', 'kóðun á A með n=3 er D');
